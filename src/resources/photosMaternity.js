a46a3c82b41a07e41beaacce7845c5ddb6b275b0
Michael R <mrwolf.lmriii@gmail.com>
2021-12-13 11:34:18 -0700

    re-addressed objects to reflect local images

--- src/resources/photosMaternity.js
+++ src/resources/photosMaternity.js
@@ -2,91 +2,101 @@ export const maternity = [
     {
         id: 1,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1545205597-ef5b9a513b6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity1.jpg'
     },
     {
         id: 2,
-        name: 'banana',
-        url: 'https://images.unsplash.com/photo-1568120677385-45695d1b8ca6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bWF0ZXJuaXR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+        name: 'pregnant woman',
+        url: '/images/maternity2.jpg'
     },
     {
         id: 3,
-        name: 'woman',
-        url: 'https://images.unsplash.com/photo-1596213281525-7811bbc7c1c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWF0ZXJuaXR5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
+        name: 'pregnant woman',
+        url: '/images/maternity3.jpg'
     },
     {
         id: 4,
-        name: 'pregnant',
-        url: 'https://images.unsplash.com/photo-1516719907742-5a390230ce74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTd8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        name: 'pregnant woman',
+        url: '/images/maternity4.jpg'
     },
     {
         id: 5,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1586630135481-3fcb73f5d8b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity5.jpg'
     },
     {
         id: 6,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1593907300483-07877e902efb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity6.jpg'
     },
     {
         id: 7,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1575401477247-9b8e684dda72?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity7.jpg'
     },
     {
         id: 8,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1569863886738-27f0348025be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njl8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity8.jpg'
     },
     {
         id: 9,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1569974666594-5967c5fbefce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity9.jpg'
     },
     {
         id: 10,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1566780592041-0a2e69c5ef09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDN8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity10.jpg'
     },
     {
         id: 11,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1554240767-b33d65050d52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity20.jpg'
     },
     {
         id: 12,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1569863887351-89dac34fd9f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODh8fG1hdGVybml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity21.jpg'
     },
     {
         id: 13,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1503555990554-fa50619a3da8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAxfHxtYXRlcm5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity22.jpg'
     },
     {
         id: 14,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1528493609210-b585031f0933?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE0fHxtYXRlcm5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity23.jpg'
     },
     {
         id: 15,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1592473995764-04b016463c52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEyfHxtYXRlcm5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity24.jpg'
     },
     {
         id: 16,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1508943050604-605048ae62c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTE4fHxtYXRlcm5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity25.jpg'
     },
     {
         id: 17,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/flagged/photo-1572531186446-87f1db5070b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI1fHxtYXRlcm5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity26.jpg'
     },
     {
         id: 18,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1543363902-9d4c1a85e67f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTIyfHxtYXRlcm5pdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=600&q=60'
+        url: '/images/maternity27.jpg'
     },
-]
+    {
+        id: 19,
+        name: 'pregnant woman',
+        url: '/images/maternity28.jpg'
+    },
+    {
+        id: 20,
+        name: 'pregnant woman',
+        url: '/images/maternity30.jpg'
+    }
+];
--- src/resources/photosNewborn.js
+++ src/resources/photosNewborn.js
@@ -3,121 +3,101 @@ export const newborn = [
     {
         id: 1,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1580301762395-21ce84d00bc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmV3Ym9ybnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn1.jpg'
     },
     {
         id: 2,
         name: 'banana',
-        url: 'https://images.unsplash.com/photo-1596252732610-fce5ac542f8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmV3Ym9ybnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn2.jpg'
     },
     {
         id: 3,
         name: 'woman',
-        url: 'https://images.unsplash.com/photo-1543342384-1f1350e27861?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn3.jpg'
     },
     {
         id: 4,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1524808533204-cda7fe65ff05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bmV3Ym9ybnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn20.jpg'
     },
     {
         id: 5,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1582486225644-aeacf6aa0b1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn21.jpg'
     },
     {
         id: 6,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1610901158532-d246c011729e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn22.jpg'
     },
     {
         id: 7,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1533483595632-c5f0e57a1936?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn23.jpg'
     },
     {
         id: 8,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1537673156864-5d2c72de7824?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn24.jpg'
     },
     {
         id: 9,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1579536745800-925dca3a7535?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn25.jpg'
     },
     {
         id: 10,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1566004100631-35d015d6a491?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjl8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn26.jpg'
     },
     {
         id: 11,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1542385151-efd9000785a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn27.jpg'
     },
     {
         id: 12,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1617331140180-e8262094733a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn28.jpg'
     },
     {
         id: 13,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1608365151231-7dbed3034787?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fG5ld2Jvcm58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn29.jpg'
     },
     {
         id: 14,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1444318226545-dfd6106d7ec4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA2fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn30.jpg'
     },
     {
         id: 15,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1542644416-2289c587843e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ4fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn31.jpg'
     },
     {
         id: 16,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1620737007484-2d3bd3079a35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUxfHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn32.jpg'
     },
     {
         id: 17,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1565278269078-59e7ac39ecc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU1fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn33.jpg'
     },
     {
         id: 18,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1610901128852-b45933ad01fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTc4fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn34.jpg'
     },
     {
         id: 19,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1515012003471-9d658cf66e1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjA0fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn35.jpg'
     },
     {
         id: 20,
         name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1510154221590-ff63e90a136f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjg4fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-        id: 21,
-        name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1560266822-3336a59dc719?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzEyfHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-        id: 22,
-        name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1545877790-63898f6ff403?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzM1fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-        id: 23,
-        name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1550831858-3c2581fed470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzQ4fHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-    },
-    {
-        id: 24,
-        name: 'pregnant woman',
-        url: 'https://images.unsplash.com/photo-1632948752104-529eda41d2bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzYwfHxuZXdib3JufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        url: '/images/newborn36.jpg'
     }
 ];
